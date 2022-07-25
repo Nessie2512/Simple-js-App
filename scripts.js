af022c8e76dfd77b0b6ffc07a9ea1ef77141cdea
Cristovao <nesselmocristovaogmail.com>
2022-07-25 11:20:17 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,96 @@
+
+
+
+/*soma = function (arg1,arg1) {
+	// body...
+
+	return arg1 + arg1
+	console.log(`a some ${arg1 + arg1}`)
+}
+
+soma(15,2)
+*/
+/*
+
+var = por ser global e afectado pelo hoisting
+ou seja se estiver dentro de um scope de bloco ela e elavada para cima e podedar um valor undefiened.
+
+let = block-scoped so funciona com no scopo onde foi criado pode ser atualizada.
+
+const =block-scoped so funciona com no scopo e nao pode ser atualizada 
+
+
+*/
+
+
+//exe01
+
+/*
+
+function numericGrade(grade){
+
+	let classifactionA = grade>=90 && grade <= 100
+	let classifactionB = grade >= 80 && grade <= 89
+	let classifactionC = grade >= 70 && grade <= 79
+	let classifactionD = grade >= 60 && grade <= 69
+	let classifactionF = grade < 60 && grade>=0
+
+
+	 
+if (classifactionA) {
+
+	console.log("Classification: A")
+}
+
+ 
+else if(classifactionB){
+
+	console.log("Classification: B")	
+
+}
+
+else if(classifactionC){
+
+	console.log("Classification: C")	
+
+}
+
+else if(classifactionD){
+
+	console.log("Classification: D")	
+
+}
+
+else if(classifactionF){
+
+	console.log("Classification: F")	
+
+}
+
+else{
+
+	console.log("Classification invalid")
+}
+
+
+
+}
+
+
+
+numericGrade(-1)
+
+
+//forOF string and array
+//forIn object
+
+*/
+
+
+let text = document.getElementsByClassName("p-footer")
+
+console.log(text)
+
+
+
+ 
